55f8ba2bece345d1f7c55373dc846c9bbd27a61b
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-06-02 13:11:48 +0200

    Diseñar las clases de misión
    -Diseñar las clases de misión (Exploración, Recolección de Datos, Colonización).
    -Incompleto, falta revisar.

---
+++ spaceshipproyect/src/main/java/com/lukasimonishvili/Main.java
@@ -0,0 +1,9 @@
+package com.lukasimonishvili;
+
+public class Main {
+    public static void main(String[] args) {
+        Mision mision1 = new MisionExploracion("Exploración de Marte", 30, 5, 1500);
+        System.out.println(mision1);
+
+    }
+}
