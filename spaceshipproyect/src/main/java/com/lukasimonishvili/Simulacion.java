4a8dee2fe2bd994358c8d0a9530632ef3b4096eb
Gabriel <sneivergm@gmail.com>
2025-06-03 14:23:22 +0200

    nuevo DatosNaves.java ; cambios en Simualacion

--- spaceshipproyect/src/main/java/com/lukasimonishvili/Simulacion.java
+++ spaceshipproyect/src/main/java/com/lukasimonishvili/Simulacion.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -19,8 +20,20 @@ public class Simulacion {
     private static final String MISION_RECOLECCION_DATOS_STRING = "mision_recoleccion_datos";
     private static final String MISION_COLONIZACION_STRING = "mision_colonizacion";
     
-    //get JSON file "DatosMisiones" and parse as JSONArray
 
+    public static NaveEspacial buscarMejorNaveApta(List<NaveEspacial> naves, int autonomiaNecesaria, String tipoExperiencia, int experienciaNecesaria) {
+        Optional<NaveEspacial> mejorNave = naves.stream()
+            // Filtrar solo las naves aptas para la misión
+            .filter(n -> n.aptasParaUnaMision(autonomiaNecesaria, tipoExperiencia, experienciaNecesaria))
+            // Ordenar según los criterios indicados (descendente autonomíaActual, descendente experiencia total, ascendente nombre)
+            .sorted(Comparator.comparingInt(NaveEspacial::getAutonomiaActual).reversed()
+                    .thenComparing(Comparator.comparingInt(NaveEspacial::getExperienciaTotal).reversed())
+                    .thenComparing(NaveEspacial::getNombre))
+            .findFirst();
+        return mejorNave.orElse(null);
+    }
+    
+    //get JSON file "DatosMisiones" and parse as JSONArray
     public static JsonArray getMissionTypes() {
         InputStream is = Simulacion.class.getClassLoader().getResourceAsStream(PATH_DATOS_MISION);
         if (is != null) {
@@ -88,6 +101,8 @@ public class Simulacion {
         List<Mision> misionesOrdenadas = sortMisionsByPriority(listMisiones);
         System.out.println("Misiones ordenadas por prioridad:");
         for (Mision m: misionesOrdenadas) System.out.println(m);
+
+
     }
 }
 
