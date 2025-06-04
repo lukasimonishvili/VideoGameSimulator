ebe521ac1a056fbaf8698a713f06057288f28e32
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2025-06-04 09:37:35 +0200

    capacidad carga

--- spaceshipproyect/src/main/java/com/lukasimonishvili/DepositoDeNaves.java
+++ spaceshipproyect/src/main/java/com/lukasimonishvili/DepositoDeNaves.java
@@ -7,10 +7,10 @@ public class DepositoDeNaves {
     public List<NaveEspacial> naves = new ArrayList<>();
 
     public DepositoDeNaves() {
-         NaveEspacial nave1 = new NaveEspacial("Nautilus", 3000, false);
-         NaveEspacial nave2 = new NaveEspacial("Space-X", 1800, true);
-         NaveEspacial nave3 = new NaveEspacial("Black pearl", 2500, true);
-         NaveEspacial nave4 = new NaveEspacial("North light", 2700, false);
+         NaveEspacial nave1 = new NaveEspacial("Nautilus", 3000, false, 2000);
+         NaveEspacial nave2 = new NaveEspacial("Space-X", 1800, true, 3000);
+         NaveEspacial nave3 = new NaveEspacial("Black pearl", 2500, true, 5000);
+         NaveEspacial nave4 = new NaveEspacial("North light", 2700, false, 2500);
 
          naves.add(nave1);
          naves.add(nave2);
@@ -27,7 +27,9 @@ public class DepositoDeNaves {
         System.out.println("está nave tiene sensores cientificos? yes/no");
         String sensoresCientificosInput = scanner.next();
         boolean sensoresCientific = sensoresCientificosInput.equals("yes");
-        NaveEspacial nuevoNaveEspacial = new NaveEspacial(nombre, autonomiaMaxima, sensoresCientific);
+        System.out.println("Por favor escriba la capacidad carga de la nave espacial");
+        int capacidadCarga = scanner.nextInt();
+        NaveEspacial nuevoNaveEspacial = new NaveEspacial(nombre, autonomiaMaxima, sensoresCientific, capacidadCarga);
         naves.add(nuevoNaveEspacial);
         System.out.println("Nueva nave espacial creada");
     }
