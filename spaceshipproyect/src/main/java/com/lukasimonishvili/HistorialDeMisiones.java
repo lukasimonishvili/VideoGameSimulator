675762dbbdcee32488f39b9adc8a99389ddf852f
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-06-05 12:58:17 +0200

    Sistema de Historial.
    Registrar resultado de la misión: éxito/fallo, nave, experiencia, evento (si hay)
    
    Consultar y mostrar historial

---
+++ spaceshipproyect/src/main/java/com/lukasimonishvili/HistorialDeMisiones.java
@@ -0,0 +1,26 @@
+package com.lukasimonishvili;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistorialDeMisiones {
+    private List<RegistroHistorial> registroHistorial =new ArrayList<>();
+   
+    public void registrarResultado(Mision mision, NaveEspacial nave, int experienciaObtenida, String eventoEspecial){
+        RegistroHistorial nuevoRegistro=new RegistroHistorial(mision, nave, experienciaObtenida, eventoEspecial);
+        registroHistorial.add(nuevoRegistro);
+        System.out.println("NUEVO REGISTRO AGREGADO");
+    }
+
+    public void mostrarHistorial(){
+        if(registroHistorial.isEmpty()){
+            System.out.println("No se encuentras registros en el historial.");
+        }else{
+            for (RegistroHistorial historial : registroHistorial) {
+                System.out.println(historial);
+            }
+        }
+    }
+
+
+}
